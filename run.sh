82f64bd2898a790c2be7ecceebd70dfea6bce9c8
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-11-28 10:22:24 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+# cd ~/plutus
+# rm -f -R -- */
+
+cd ~/fortuna
+rm -f -R -- */
+
+git clone https://github.com/mintomohan/hellopython
+cd hellopython
+python3 hello.py
+
+# cd ~/plutus
+# sh -x wgetYrFin.sh
+
+exit
