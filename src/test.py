19a3b4ba8a6607436bdcb93702b658f2046f4ee0
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-11-28 14:37:54 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@ import os
 
 fileDir = os.path.dirname(os.path.realpath('__file__'))
 print(fileDir)
+print(os.path.join(fileDir, '../config/fortuna.properties'))
 
 props = dict( l.rstrip().split('=') for l in open("/home/ec2-user/fortuna/fortuna/config/fortuna.properties")
   if not l.startswith("#") )
