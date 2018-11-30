73342fbd55123e193c4d0a3e2fd0af35d4009774
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-11-30 11:38:46 +0530

    Update sendmail.py

--- src/sendmail.py
+++ src/sendmail.py
@@ -1,12 +1,20 @@
+import smtplib
+import fortunacommon
 
+pr=fortunacommon.loadAppProperties()
+gmail_user=pr['mail.user.id']
+gmail_password=pr['mail.user.password']
+to=pr['mail.recepient.address']
+subject="[Fortuna]: test mail"
+body="This is an automated mail message."
 
-email_text = "From: %s\nTo: %s\nSubject: %s\n\n%s" % (sent_from, to, subject, body)
+email_text = "From: %s\nTo: %s\nSubject: %s\n\n%s" % (gmail_user, to, subject, body)
 
 try:  
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
     server.login(gmail_user, gmail_password)
-    server.sendmail(sent_from, to, email_text)
+    server.sendmail(gmail_user, to, email_text)
     server.close()
 
     print('Email sent!')
