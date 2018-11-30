f048be9b657f63862e9ca4f3f564f4b1d394abca
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-11-30 14:20:26 +0530

    Update run.sh

--- shell/run.sh
+++ shell/run.sh
@@ -1,15 +1,19 @@
-# cd ~/plutus
-# rm -f -R -- */
+cd ~/plutus
+rm -f -R -- */
 
 cd ~/fortuna
 rm -f -R -- */
 
 git clone https://github.com/pyfortuna/fortuna
 cp ~/fortuna.properties fortuna/config
-cd fortuna/src
-python3 hello.py
 
-# cd ~/plutus
-# sh -x wgetYrFin.sh
+cd ~/fortuna/fortuna/src
+python3 generateShell.py
+
+cd ~/plutus
+sh -x wgetYrFin.sh
+
+cd ~/fortuna/fortuna/src
+python3 processFinYr.py
 
 exit
