59ce50dfa3e51bf8ae14ef3e82dfdcd729d8d09e
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-11-30 11:16:43 +0530

    Update fortunacommon.py

--- src/fortunacommon.py
+++ src/fortunacommon.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 # -----------------------------------------------------------------------------
 # Load property key/values from "fortuna.properties" file in "config" directory
@@ -9,3 +10,10 @@ def loadAppProperties():
   props = dict( l.rstrip().split('=') for l in open(propfile)
     if not l.startswith("#") )
   return props
+
+# ---------------------------------------------------------------
+# Get list of filenames in a directory which matches with pattern
+# ---------------------------------------------------------------
+def getFiles(inputDirectory,filenameRegexPattern):
+  files = [f for f in os.listdir(inputDirectory) if re.match(filenameRegexPattern, f)]
+  return files
