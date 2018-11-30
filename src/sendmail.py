05c0d091761d01d384bcaa55baf194388a3626b3
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-11-30 12:04:22 +0530

    Update sendmail.py

--- src/sendmail.py
+++ src/sendmail.py
@@ -1,4 +1,5 @@
 import smtplib
+import os
 import fortunacommon
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -25,13 +26,24 @@ try:
     msg.attach(content)
    
     # Attachment
+    #attachmentFilename = pr['finyr.output.filename']
+    #print(attachmentFilename)
+    #attachmentFile = file(attachmentFilename)
+    #print(attachmentFile)
+    #attachment = MIMEText(attachmentFile.read())
+    #attachment.add_header('Content-Disposition', 'attachment', filename=attachmentFilename)           
+    #msg.attach(attachment)
+    
+    #
     attachmentFilename = pr['finyr.output.filename']
     print(attachmentFilename)
     attachmentFile = file(attachmentFilename)
-    print(attachmentFile)
-    attachment = MIMEText(attachmentFile.read())
-    attachment.add_header('Content-Disposition', 'attachment', filename=attachmentFilename)           
+    attachment = MIMEBase('application', 'octet-stream')
+    attachment.set_payload(open(attachmentFile, 'rb').read())
+    encoders.encode_base64(attachment)
+    attachment.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(attachmentFile))
     msg.attach(attachment)
+
     
     server.sendmail(gmail_user, recipient_address, msg.as_string())
     server.close()    
