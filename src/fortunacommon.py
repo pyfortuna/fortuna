b91682cc68dfd27b65124c636aee3ee7600cad18
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-11-30 10:59:28 +0530

    Create fortunacommon.py

---
+++ src/fortunacommon.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+# Load property key/values from "fortuna.properties" file in "config" directory
+# -----------------------------------------------------------------------------
+def loadAppProperties():
+  fileDir = os.path.dirname(os.path.abspath(__file__))
+  propfile = os.path.join(fileDir, '../config/fortuna.properties')
+  props = dict( l.rstrip().split('=') for l in open(propfile)
+    if not l.startswith("#") )
+  return props
