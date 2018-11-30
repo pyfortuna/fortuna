8096612186e634b2eafe00e09e09b07617eb7e6d
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-11-30 12:38:07 +0530

    Update sendmail.py

--- src/sendmail.py
+++ src/sendmail.py
@@ -7,50 +7,46 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
+def sendMail(subject,body,attachmentFilename):
+    pr=fortunacommon.loadAppProperties()
+    gmail_user=pr['mail.user.id']
+    gmail_password=pr['mail.user.password']
+    recipient_address=pr['mail.recepient.address']    
+
+    try:  
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+
+        # Message
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = gmail_user
+        content = MIMEText(body, 'plain')
+        msg.attach(content)
+
+        # Add attachment        
+        with open(attachmentFilename, "rb") as attachmentFile:
+            part = MIMEApplication(
+                attachmentFile.read(),
+                Name=basename(attachmentFilename)
+            )
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(attachmentFilename)
+        msg.attach(part)
+
+        # Send mail
+        server.sendmail(gmail_user, recipient_address, msg.as_string())
+        server.close()    
+
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
+
+# ------------
+# Main program
+# ------------
 pr=fortunacommon.loadAppProperties()
-gmail_user=pr['mail.user.id']
-gmail_password=pr['mail.user.password']
-recipient_address=pr['mail.recepient.address']
 subject="[Fortuna]: Yearly Financial results"
 body="This is an automated e-mail message sent from Fortuna."
-
-#email_text = "From: %s\nTo: %s\nSubject: %s\n\n%s" % (gmail_user, to, subject, body)
-
-try:  
-    msg = MIMEMultipart('alternative')
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(gmail_user, gmail_password)
-    
-    # Message
-    msg['Subject'] = subject
-    msg['From'] = gmail_user
-    content = MIMEText(body, 'plain')
-    msg.attach(content)
-   
-    # Attachment
-    #attachmentFilename = pr['finyr.output.filename']
-    #print(attachmentFilename)
-    #attachmentFile = file(attachmentFilename)
-    #print(attachmentFile)
-    #attachment = MIMEText(attachmentFile.read())
-    #attachment.add_header('Content-Disposition', 'attachment', filename=attachmentFilename)           
-    #msg.attach(attachment)
-    
-    #
-    attachmentFilename = pr['finyr.output.filename']
-    with open(attachmentFilename, "rb") as attachmentFile:
-        part = MIMEApplication(
-            attachmentFile.read(),
-            Name=basename(attachmentFilename)
-        )
-    # After the file is closed
-    part['Content-Disposition'] = 'attachment; filename="%s"' % basename(attachmentFilename)
-    msg.attach(part)
-    
-    server.sendmail(gmail_user, recipient_address, msg.as_string())
-    server.close()    
-    
-    print('Email sent!')
-except:
-    print('Something went wrong...')
+attachmentFilename = pr['finyr.output.filename']
+sendMail(subject,body,attachmentFilename)
