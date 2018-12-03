2ba08e01675884fa40e29f55d31aea4a9a989c61
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-03 10:00:28 +0530

    Update generateShell.py

--- src/generateShell.py
+++ src/generateShell.py
@@ -1,29 +1,6 @@
 import re
 import fortunacommon
 
-class Moneycontrol:
-  def __init__(self, url):
-    self.url = url
-    regex = r"http[s]{0,1}://www.moneycontrol.com/india/stockpricequote/(.*)/(.*)/(.*)"
-    if re.search(regex, url):
-      m=re.search(regex, url)
-      self.category = m.group(1)
-      self.companyname = m.group(2)
-      self.companycode = m.group(3)
-  def getURL(self):
-    return self.url
-  def getCompanyName(self):
-    return self.companyname
-  def getYrFinURL(self):
-    yrFinURL = "https://www.moneycontrol.com/financials/" + self.companyname + "/results/yearly/" + self.companycode
-    return yrFinURL
-  def getQtrFinURL(self):
-    qtrFinURL = "https://www.moneycontrol.com/financials/" + self.companyname + "/results/quarterly-results/" + self.companycode
-    return qtrFinURL
-  def getRatioURL(self):
-    ratioURL = "https://www.moneycontrol.com/financials/" + self.companyname + "/ratiosVI/" + self.companycode
-    return ratioURL
-
 # --------------------------------------------------------
 # Main program
 # --------------------------------------------------------
@@ -36,7 +13,7 @@ f.close()
 fYrFin= open(pr['genshell.output.finyr.filename'],"w+")
 
 for mcListItem in mcList:
-  mc = Moneycontrol(mcListItem)
+  mc = fortunacommon.Moneycontrol(mcListItem)
   yrFinGetCommand = "wget " + mc.getYrFinURL() + " -O " + pr['finyr.input.directory'] + mc.getCompanyName() + ".html" + "\n"
   fYrFin.write(yrFinGetCommand)
 
