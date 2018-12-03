41d4ae9c61b46b9c858fa906cda922f0502b46fa
EC2 Default User <ec2-user@ip-172-31-37-41.us-east-2.compute.internal>
2018-12-03 15:34:21 +0000

    added regex

--- src/test.py
+++ src/test.py
@@ -1,8 +1,11 @@
 import fortunacommon
+import re
 
 # --------------------------------------------------------
 # Main program
 # --------------------------------------------------------
+
+companyNameRegex="(.*)\s\(\d{1,3}\)"
 
 pr=fortunacommon.loadAppProperties()
 f = open('/home/ec2-user/fortuna/fortuna/data/pf.txt', 'r')
@@ -13,7 +16,13 @@ i = 0
 while i < len(pfList):
 	print("==========================")
 	print(str(i) + " : " + pfList[i])
+	if re.search(companyNameRegex,str(pfList[i])):
+		m=re.search(companyNameRegex,pfList[i])
+		companyName=m.group(1)
+		print("Company : " + companyName)
 	print(str(i) + " : " + pfList[i+2])
+	sector=pfList[i+2]
+	print("Sector : " + sector)
 	print(str(i) + " : " + pfList[i+4])
 	i += 5
 print("-----------------------")
