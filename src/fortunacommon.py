f94492f3335575f6e1eba74d39835095b7e1c79a
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-03 10:01:48 +0530

    Update fortunacommon.py

--- src/fortunacommon.py
+++ src/fortunacommon.py
@@ -7,6 +7,32 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
+# ----------------------------------
+# Class for parsing Moneycontrol URL
+# ----------------------------------
+class Moneycontrol:
+  def __init__(self, url):
+    self.url = url
+    regex = r"http[s]{0,1}://www.moneycontrol.com/india/stockpricequote/(.*)/(.*)/(.*)"
+    if re.search(regex, url):
+      m=re.search(regex, url)
+      self.category = m.group(1)
+      self.companyname = m.group(2)
+      self.companycode = m.group(3)
+  def getURL(self):
+    return self.url
+  def getCompanyName(self):
+    return self.companyname
+  def getYrFinURL(self):
+    yrFinURL = "https://www.moneycontrol.com/financials/" + self.companyname + "/results/yearly/" + self.companycode
+    return yrFinURL
+  def getQtrFinURL(self):
+    qtrFinURL = "https://www.moneycontrol.com/financials/" + self.companyname + "/results/quarterly-results/" + self.companycode
+    return qtrFinURL
+  def getRatioURL(self):
+    ratioURL = "https://www.moneycontrol.com/financials/" + self.companyname + "/ratiosVI/" + self.companycode
+    return ratioURL
+
 # -----------------------------------------------------------------------------
 # Load property key/values from "fortuna.properties" file in "config" directory
 # -----------------------------------------------------------------------------
