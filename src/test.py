f3e5beb5f9d2eedab8deb2df3224a39340c1cbc1
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-03 13:01:41 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -5,14 +5,14 @@ import fortunacommon
 # --------------------------------------------------------
 
 pr=fortunacommon.loadAppProperties()
-f = open('/user/ec2-user/fortuna/fortuna/data/pf.txt', 'r')
+f = open('/home/ec2-user/fortuna/fortuna/data/pf.txt', 'r')
 pfList = f.read().splitlines()
 f.close()
 
 i = 0
 while i < len(pfList):
-	print("=========================="])
-	print(i + " : " + pfList[i])
-	print(i + " : " + pfList[i+2])
-	print(i + " : " + pfList[i+4])
+	print("==========================")
+	print(str(i) + " : " + pfList[i])
+	print(str(i) + " : " + pfList[i+2])
+	print(str(i) + " : " + pfList[i+4])
 	i += 5
