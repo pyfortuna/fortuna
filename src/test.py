8062c50190559d1d2df347d6d070e99ce3031487
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-04 14:48:02 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,5 +1,7 @@
+# Import libraries
 import fortunacommon
-import re
+import re   # for Regex
+import csv  # for CSV file creation
 
 # --------------------------------------------------------
 # Main program
@@ -12,38 +14,31 @@ f = open('/home/ec2-user/fortuna/fortuna/data/pf.txt', 'r')
 pfList = f.read().splitlines()
 f.close()
 
-i = 0
-while i < len(pfList):
-	print("==========================")
-	#print(str(i) + " : " + pfList[i])
-	if re.search(companyNameRegex,str(pfList[i])):
-		m=re.search(companyNameRegex,pfList[i])
-		companyName=m.group(1)
-		print("Company : " + companyName)
-	#print(str(i) + " : " + pfList[i+2])
-	sector=pfList[i+2]
-	print("Sector : " + sector)
-	#print(str(i) + " : " + pfList[i+4])
-	l=pfList[i+4].split("\t")
-	#print(str(l))
-	livePrice=l[0]
-	priceChange=l[1].replace("+","")
-	quantity=l[2]
-	invPrice=l[3].replace(",","")
-	daysGain=l[4].replace(",","")
-	daysGainPercent=l[5]
-	totalGain=l[6].replace(",","")
-	totalGainPercent=l[7]
-	currentValue=l[8].replace(",","")
-	print("Live Price : " + livePrice)
-	print("Change : " + priceChange)
-	print("Quantity : " + quantity)
-	print("Investment Price : " + invPrice)
-	print("Day's Gain : " + daysGain)
-	print("Day's Gain % : " + daysGainPercent)
-	print("Total Gain : " + totalGain)
-	print("Total Gain % : " + totalGainPercent)
-	print("Current Value : " + currentValue)
-	
-	i += 5
-print("-----------------------")
+# process pf file and create csv file
+with open('/home/ec2-user/plutus/data/pf.csv', 'w') as csvfile:
+	fieldnames = ['companyName', 'sector', 'livePrice', 'priceChange', 'quantity', 'invPrice','daysGain','daysGainPercent','totalGain','totalGainPercent','currentValue']
+	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	writer.writeheader()
+
+	# Read pf file
+	i = 0
+	while i < len(pfList):
+		if re.search(companyNameRegex,str(pfList[i])):
+			m=re.search(companyNameRegex,pfList[i])
+			companyName=m.group(1)
+		sector=pfList[i+2]
+		l=pfList[i+4].split("\t")
+		livePrice=l[0]
+		priceChange=l[1].replace("+","")
+		quantity=l[2]
+		invPrice=l[3].replace(",","")
+		daysGain=l[4].replace(",","")
+		daysGainPercent=l[5]
+		totalGain=l[6].replace(",","")
+		totalGainPercent=l[7]
+		currentValue=l[8].replace(",","")
+
+		writer.writerow({'companyName':companyName,'sector':sector,'livePrice':livePrice,'priceChange':priceChange,'quantity':quantity,'invPrice':invPrice,'daysGain':daysGain,'daysGainPercent':daysGainPercent,'totalGain':totalGain,'totalGainPercent':totalGainPercent,'currentValue':currentValue})
+
+		i += 5
+print("FORTUNA: pf.csv file created")
