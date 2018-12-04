50ecba9b778887f6d11a125b06347ae26aa898dc
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-04 17:25:10 +0530

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,6 @@
+# Import libraries
+import fortunacommon
+import re   # for Regex
+
+# Regular expressions
+dataRegex="itemprop=\"name\">[\"Buy\"|\"Sell\"].*?</a></h3><time class=\"date-format\" data-time=\".*?\">"
