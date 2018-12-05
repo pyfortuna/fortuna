19b7f2ec015cceb9f9836b53ccd88131428c888d
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-05 18:01:19 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 from urllib.request import urlopen
 
 dfPF = pd.read_csv("/home/ec2-user/plutus/pf.csv")[['companyName','currentValue']]
@@ -6,7 +7,7 @@ dfFinYr = pd.read_csv("/home/ec2-user/plutus/finYr.csv")[['companyShortName','pl
 
 res=pd.merge(dfPF, dfFinYr, left_on=['companyName'], right_on=['companyShortName'])
 res1=res.sort_values(by='eps_coef', ascending=False)
-print(res1)
+#print(res1)
 
 
 # https://docs.python.org/dev/tutorial/stdlib.html#internet-access
@@ -15,3 +16,11 @@ with urlopen('https://www.moneycontrol.com/india/stockpricequote/infrastructure-
 		line = line.decode('utf-8')  # Decoding the binary data to text.
 		if 'Nse_Prc_tick' in line:  # look for NSE Price
 			print(line)
+			m=re.search("<strong>(.*?)</strong>",line)
+			livePrice=m.group(1)
+			print("livePrice : " + livePrice)
+		elif 'compname_imp' in line:  # look for Company Name
+			print(line)
+			m=re.search("value=\"(.*?)\"",line)
+			companyName=m.group(1)
+			print("companyName : " + companyName)
