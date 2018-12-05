23484fd3954e711f2fdfb7dae460c61e8dd3e64f
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-05 14:00:31 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 # Import libraries
 import fortunacommon
 import re   # for Regex
-#from datetime import datetime # for date parsing and formatting
+from datetime import datetime # for date parsing and formatting
 
 # Regular expressions
 dataRegex="itemprop=\"name\">[\"Buy\"|\"Sell\"].*?</a></h3><time class=\"date-format\" data-time=\".*?\">"
@@ -28,6 +28,6 @@ for (idx, matchData) in enumerate(re.findall(pattern, etrecoData), start=1):
     price=m.group(3).strip()
     recommender=m.group(4).strip()
     dateTimeStr=m.group(5).strip()
-    #datetime_object = datetime.strptime(dateTimeStr, "%b %d, %Y, %I:%M %p %Z")
+    datetime_object = datetime.strptime(dateTimeStr, "%b %d, %Y, %I:%M %p %Z")
     #dateTimeVal = datetime_object.strftime("%d-%b-%Y")
     print(recoType + "\t" + companyName + "\t" + price + "\t" + recommender + "\t" + dateTimeStr)
