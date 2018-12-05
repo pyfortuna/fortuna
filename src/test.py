27f0a65172ba2d5a9677bfe7786750716ee47938
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-05 17:52:51 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,10 +1,17 @@
 import pandas as pd
+from urllib.request import urlopen
 
 dfPF = pd.read_csv("/home/ec2-user/plutus/pf.csv")[['companyName','currentValue']]
 dfFinYr = pd.read_csv("/home/ec2-user/plutus/finYr.csv")[['companyShortName','pl_coef','eps_coef']]
-print(dfPF)
-print(dfFinYr)
 
 res=pd.merge(dfPF, dfFinYr, left_on=['companyName'], right_on=['companyShortName'])
 res1=res.sort_values(by='eps_coef', ascending=False)
 print(res1)
+
+
+# https://docs.python.org/dev/tutorial/stdlib.html#internet-access
+with urlopen('https://www.moneycontrol.com/india/stockpricequote/infrastructure-general/abbindia/ABB') as response:
+	for line in response:
+		line = line.decode('utf-8')  # Decoding the binary data to text.
+		if 'Nse_Prc_tick' in line:  # look for NSE Price
+			print(line)
