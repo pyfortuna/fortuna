67aab2e6a0819bc167fd1268bf50ecc0f03c8829
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-05 16:45:08 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 dfPF = pd.read_csv("/home/ec2-user/plutus/pf.csv")
+dfFinYr = pd.read_csv("/home/ec2-user/plutus/FinYr.csv")
 print(dfPF)
+print(dfFinYr)
+
+res=pd.merge(dfPF, dfFinYr, left_on=['companyName'], right_on=['companyName'])
+print(res)
