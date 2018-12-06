e7a159a3f8307f0526e1f394f2c6c9aaa2fd2f91
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-06 17:36:07 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,7 +1,11 @@
 import fortunacommon
 
-pr=fortunacommon.loadProperties()
+pr=fortunacommon.loadAppProperties()
 
 quandlURL="https://www.quandl.com/api/v3/datasets/NSE/ASHOKLEY.csv?api_key=" + pr['quandl.api.key']
 
 print(quandlURL)
+
+with urlopen(quandlURL) as response:
+  for line in response:
+    print(line)
