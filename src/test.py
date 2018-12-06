861fb9440571b365f101f5bc8eca79cba90c679e
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-06 18:18:54 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,8 +1,10 @@
 import fortunacommon
-from urllib.request import urlopen
+import urllib.request
+import pandas as pd
 
 pr=fortunacommon.loadAppProperties()
 
+'''
 quandlURL="https://www.quandl.com/api/v3/datasets/NSE/ASHOKLEY.csv?api_key=" + pr['quandl.api.key']
 
 print(quandlURL)
@@ -11,3 +13,13 @@ with urlopen(quandlURL) as response:
   for line in response:
     dataLine=str(line.strip()).replace("b","").replace("'","")
     print(dataLine)
+'''
+
+nseURL="https://www.nseindia.com/products/dynaContent/common/productsSymbolMapping.jsp?symbol=ASHOKLEY&segmentLink=3&symbolCount=1&series=ALL&dateRange=12month&fromDate=&toDate=&dataType=PRICEVOLUMEDELIVERABLE"
+
+fp = urllib.request.urlopen(nseURL)
+res = fp.read()
+
+history_df = pd.read_html(res, header=0, index_col='Date')[0]
+
+print(history_df)
