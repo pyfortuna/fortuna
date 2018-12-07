5a388ca38029da9f9994ee9f4c8b833f38fe6494
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-07 18:51:22 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -65,13 +65,18 @@ sma_df=input_df.rolling(window=30).mean().round(2).diff()
 #print(sma_df.query('close > 0'))
 print(sma_df.query('close == close'))
 
-print(boll_df)
+# Bollinger Bands
+#  https://en.wikipedia.org/wiki/Bollinger_Bands
+#  https://github.com/bukosabino/ta/blob/5e8e92bd9e41eec559a3dd849d1bfe9143cf84ae/ta/volatility.py
 boll_df=history_df[['close']]
+print(boll_df)
+
 mavg = boll_df['close'].rolling(20).mean()
 mstd = boll_df['close'].rolling(20).std()
 hband = mavg + 2*mstd
 lband = mavg - 2*mstd
 boll_df['hband']=hband
 boll_df['lband']=lband
+
 print(boll_df)
 
