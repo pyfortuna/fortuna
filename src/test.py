553ed3c5b57599b35e0c04c4cdd668a35afc0867
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-07 13:50:50 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -50,4 +50,4 @@ with urllib.request.urlopen(req) as response:
 history_df = pd.read_html(the_page, header=0, index_col='Date')[0]
 
 print(history_df)
-print(list(history_df))
+print(list(history_df[['Open Price','High Price','Low Price','Close Price','VWAP']]))
