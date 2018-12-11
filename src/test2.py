3fd00685658d53588b0ba660bfb602e361e287c6
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-11 09:48:41 +0530

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -1,4 +1,6 @@
 import datetime
+import time
+
 
 now = datetime.datetime.now()
 print(now)
@@ -7,10 +9,16 @@ print(d1)
 d2 = d1 - datetime.timedelta(days=365)
 print(d2)
 
-'''
-start=
+
+
+start=d2
+end=d1
 difference = (end - start).days
-if difference > 100:
-curr_end = start + timedelta(days=100)
-while curr_end < end:
-'''
+  if difference > 100:
+    curr_end = start + timedelta(days=100)
+    while curr_end < end:
+      start_fmt=time.strftime("%d-%m-%Y", start)
+      end_fmt=time.strftime("%d-%m-%Y", curr_end)
+      print(start_fmt + " to " + end_fmt)
+      start = curr_end + timedelta(days=1)
+      curr_end += timedelta(days=100)
