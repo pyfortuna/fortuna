f2e8e18969bbc23ee2506e18e30339384f535fd6
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-11 10:13:39 +0530

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -5,22 +5,25 @@ import time
 end=datetime.datetime.now()
 start=end - datetime.timedelta(days=365)
 
+dates = {}
+datesList = []
 difference = (end - start).days
 if difference > 100:
   curr_end = start + datetime.timedelta(days=100)
   while curr_end < end:
-    start_fmt=start.strftime("%d-%m-%Y")
-    end_fmt=curr_end.strftime("%d-%m-%Y")
-    print(start_fmt + " to " + end_fmt)
+    dates['start']=start.strftime("%d-%m-%Y")
+    dates['end']=curr_end.strftime("%d-%m-%Y")
+    datesList.append(dates)
     start = curr_end + datetime.timedelta(days=1)
     curr_end += datetime.timedelta(days=100)
   if curr_end > end:
     #start = curr_end + datetime.timedelta(days=1)
-    start_fmt=start.strftime("%d-%m-%Y")
-    end_fmt=end.strftime("%d-%m-%Y")
-    print(start_fmt + " to " + end_fmt)
+    dates['start']=start.strftime("%d-%m-%Y")
+    dates['end']=curr_end.strftime("%d-%m-%Y")
+    datesList.append(dates)
 else:
-  start_fmt=start.strftime("%d-%m-%Y")
-  end_fmt=end.strftime("%d-%m-%Y")
-  print(start_fmt + " to " + end_fmt)
+  dates['start']=start.strftime("%d-%m-%Y")
+  dates['end']=curr_end.strftime("%d-%m-%Y")
+  datesList.append(dates)
   
+print(datesList)
