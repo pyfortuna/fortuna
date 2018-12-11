cecac44c37dc3d9d362bc8ee5678c59866881238
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-11 10:15:06 +0530

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -5,12 +5,13 @@ import time
 end=datetime.datetime.now()
 start=end - datetime.timedelta(days=365)
 
-dates = {}
+
 datesList = []
 difference = (end - start).days
 if difference > 100:
   curr_end = start + datetime.timedelta(days=100)
   while curr_end < end:
+    dates = {}
     dates['start']=start.strftime("%d-%m-%Y")
     dates['end']=curr_end.strftime("%d-%m-%Y")
     datesList.append(dates)
@@ -18,10 +19,12 @@ if difference > 100:
     curr_end += datetime.timedelta(days=100)
   if curr_end > end:
     #start = curr_end + datetime.timedelta(days=1)
+    dates = {}
     dates['start']=start.strftime("%d-%m-%Y")
     dates['end']=curr_end.strftime("%d-%m-%Y")
     datesList.append(dates)
 else:
+  dates = {}
   dates['start']=start.strftime("%d-%m-%Y")
   dates['end']=curr_end.strftime("%d-%m-%Y")
   datesList.append(dates)
