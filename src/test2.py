c6176f91357684c826222c85cc46c6b931912217
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-11 10:21:59 +0530

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -5,29 +5,31 @@
 import datetime
 import time
 
-
-end=datetime.datetime.now()
-start=end - datetime.timedelta(days=365)
-NSE_DATE_FMT="%d-%m-%Y"
-
-dates={}
-datesList = []
-difference = (end - start).days
-if difference > 100:
-  curr_end = start + datetime.timedelta(days=100)
-  while curr_end < end:
+def get100DayList(start, end):
+  NSE_DATE_FMT="%d-%m-%Y"
+  dates={}
+  datesList = []
+  difference = (end - start).days
+  if difference > 100:
+    curr_end = start + datetime.timedelta(days=100)
+    while curr_end < end:
+      dates['start']=start.strftime(NSE_DATE_FMT)
+      dates['end']=curr_end.strftime(NSE_DATE_FMT)
+      datesList.append(dates.copy())
+      start = curr_end + datetime.timedelta(days=1)
+      curr_end += datetime.timedelta(days=100)
+    if curr_end > end:
+      dates['start']=start.strftime(NSE_DATE_FMT)
+      dates['end']=end.strftime(NSE_DATE_FMT)
+      datesList.append(dates.copy())
+  else:
     dates['start']=start.strftime(NSE_DATE_FMT)
     dates['end']=curr_end.strftime(NSE_DATE_FMT)
     datesList.append(dates.copy())
-    start = curr_end + datetime.timedelta(days=1)
-    curr_end += datetime.timedelta(days=100)
-  if curr_end > end:
-    dates['start']=start.strftime(NSE_DATE_FMT)
-    dates['end']=end.strftime(NSE_DATE_FMT)
-    datesList.append(dates.copy())
-else:
-  dates['start']=start.strftime(NSE_DATE_FMT)
-  dates['end']=curr_end.strftime(NSE_DATE_FMT)
-  datesList.append(dates.copy())
-  
-print(datesList)
+  return datesList
+
+
+e = datetime.datetime.now()
+s = e - datetime.timedelta(days=365)
+x = get100DayList(start,end)
+print(x)
