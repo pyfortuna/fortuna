cb59fcc756db78c633dcef373662bdd26479be69
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-11 09:31:42 +0530

    Create test2.py

---
+++ src/test2.py
@@ -0,0 +1,14 @@
+import datetime
+
+now = datetime.datetime.now()
+print(now)
+d=date.today()
+print(d)
+
+'''
+start=
+difference = (end - start).days
+if difference > 100:
+curr_end = start + timedelta(days=100)
+while curr_end < end:
+'''
