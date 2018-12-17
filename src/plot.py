ea2cb512eaf5b971825fe1ee1eb43f79f24e8b68
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-17 12:53:20 +0530

    Update plot.py

--- src/plot.py
+++ src/plot.py
@@ -2,12 +2,12 @@ print("-----")
 import plotly.plotly as py
 import plotly.graph_objs as go
 import pandas as pd
-from datetime import datetime
+import datetime
 import fortunacommon as fc
 import test as tt
 
-e = datetime.now()
-s = e - timedelta(days=30)
+e = datetime.datetime.now()
+s = e - datetime.timedelta(days=30)
 history_df = tt.getHistoricData('ASHOKLEY',s,e)
 
 trace = go.Ohlc(x=history_df.index, open=history_df.open, high=history_df.high, low=history_df.low, close=history_df.close)
