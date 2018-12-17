fb6f6b421b2be5ea86af22e50f5cbcc0a9f6447f
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-17 12:14:28 +0530

    Update plot.py

--- src/plot.py
+++ src/plot.py
@@ -4,10 +4,14 @@ from plotly.tools import FigureFactory as FF
 from datetime import datetime
 import pandas.io.data as web
 import fortunacommon as fc
+import test as tt
 
-df = web.DataReader("aapl", 'yahoo', datetime(2007, 10, 1), datetime(2009, 4, 1))
+e = datetime.datetime.now()
+s = e - datetime.timedelta(days=30)
+dayList = tt.get100DayList(s,e)
+history_df = tt.getHistoricData('ASHOKLEY',s,e)
 
-fig = FF.create_candlestick(df.Open, df.High, df.Low, df.Close, dates=df.index)
+fig = FF.create_candlestick(history_df.ppen, history_df.high, history_df.low, history_df.close, dates=history_df.index)
 
 py.iplot(fig, filename='plutus/plotlytest.png', validate=False)
 
