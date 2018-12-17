6877fcef7a3c42cea877c27c4ae1475a7e40d5a7
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-17 18:35:27 +0530

    Update candleplot.py

--- src/candleplot.py
+++ src/candleplot.py
@@ -8,8 +8,13 @@ from datetime import datetime
 
 
 def plotCandlestick(df,filename):
+  ohlc = []
+  for index, row in df.iterrows():
+    rec = date2num(datetime.fromisoformat(row['Date'])), row['AAPL.Open'], row['AAPL.High'], row['AAPL.Low'], row['AAPL.Close']
+    ohlc.append(rec)
+    x+=1  
   fig, ax = plt.subplots()
-  ohlc=zip(date2num(datetime.fromisoformat(df['Date'])),df['AAPL.Open'], df['AAPL.High'],df['AAPL.Low'], df['AAPL.Close'])
+  #ohlc=zip(date2num(datetime.fromisoformat(df['Date'])),df['AAPL.Open'], df['AAPL.High'],df['AAPL.Low'], df['AAPL.Close'])
   print(ohlc)
   candlestick_ohlc(ax, ohlc)
   #candlestick2_ohlc(ax, df['AAPL.Open'], df['AAPL.High'],df['AAPL.Low'], df['AAPL.Close'])
@@ -19,6 +24,7 @@ def plotCandlestick(df,filename):
   #fig.autofmt_xdate()
   fig.savefig(filename)
 
+# MAIN
 o_file="/home/ec2-user/plutus/candleplot.png"
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
 plotCandlestick(df,o_file)
