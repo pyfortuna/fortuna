591d5e5f4ea154681978234968c1aee8b272e368
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2018-12-28 17:16:14 +0530

    Update xirr.py

--- src/xirr.py
+++ src/xirr.py
@@ -40,7 +40,7 @@ def getCashFlowData(df):
 
 def processPF(df):
 	companyList=df.company.unique().tolist()
-	companyList=['Reliance','TCS','KRBL']
+	#companyList=['Reliance','TCS','KRBL']
 	xirrList=[]
 	for companyName in companyList:
 		dfCF=df[df.company==companyName]
