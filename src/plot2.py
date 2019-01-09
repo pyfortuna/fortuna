a8f9bd59bf04671a0278605e26c757539ed6ad37
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-09 12:32:50 +0530

    Update plot2.py

--- src/plot2.py
+++ src/plot2.py
@@ -29,9 +29,9 @@ print(df.tail())
 fig, ax = plt.subplots()
 ax.plot_date(df.index.values,df['sma200'].values, fmt='b-', label='SMA/200')
 ax.plot_date(df.index.values,df['close'].values, fmt='r-', label=companyCode)
-#ax.xaxis.set_major_locator(plt.MaxNLocator(3))
 ax.xaxis_date()
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%Y'))
+ax.xaxis.set_major_locator(plt.MaxNLocator(6))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
 plt.savefig('/home/ec2-user/plutus/plotsample002.png')
 
 # Send mail
