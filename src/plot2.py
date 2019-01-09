20c2b9aebcf8c0efcfb510f18a0b42d77f893a29
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-09 17:17:02 +0530

    Update plot2.py

--- src/plot2.py
+++ src/plot2.py
@@ -16,8 +16,7 @@ smaFilename='/home/ec2-user/plutus/smaplot003.png'
 # Data preparation
 e = datetime.datetime.now()
 s = e - datetime.timedelta(days=numDays)
-dfHist = nu.getHistoricPrice(companyCode,s,e)
-df=dfHist[['close']]
+df = nu.getHistoricPrice(companyCode,s,e)
 sma200 = df['close'].rolling(200).mean()
 sma20 = df['close'].rolling(20).mean()
 mstd = df['close'].rolling(20).std()
@@ -45,7 +44,7 @@ ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=[3,6,9,12]))
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
 plt.savefig(smaFilename, dpi=300)
 
-df1=dfHist.tail(10)
+df1=dfHist.tail(15)
 #cp.plotCandlestick(df1,candleFilename,'%Y-%m-%d')
 cp.plotCandlestick(df1,candleFilename)
 
