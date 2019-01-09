f434afd425a4e390b8fff89bab19855fd30870e2
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-09 14:03:02 +0530

    Update plot2.py

--- src/plot2.py
+++ src/plot2.py
@@ -8,29 +8,30 @@ import matplotlib.dates as mdates
 
 # Configure parameters
 companyCode='ASHOKLEY'
+numDays=700
 e = datetime.datetime.now()
-s = e - datetime.timedelta(days=700)
+s = e - datetime.timedelta(days=numDays)
 
 # Data preparation
 df = nu.getHistoricPrice(companyCode,s,e)
-boll_df=df[['close']]
 sma200 = df['close'].rolling(200).mean()
+sma20 = df['close'].rolling(20).mean()
 df=df.assign(sma200=sma200)
+df=df.assign(sma20=sma20)
 df=df.dropna()
-#df = df.reset_index()
-df = df[['sma200','close']]
+df = df[['sma200','sma20','close']]
 df['date'] = pd.to_datetime(df.index, format="%d-%b-%Y")
 df=df.set_index('date')
-
 print(df.head())
 print(df.tail())
 
 # Plot chart
 fig, ax = plt.subplots()
 ax.plot_date(df.index.values,df['sma200'].values, fmt='b-', label='SMA/200')
+ax.plot_date(df.index.values,df['sma20'].values, fmt='g-', label='SMA/20')
 ax.plot_date(df.index.values,df['close'].values, fmt='r-', label=companyCode)
+ax.legend()
 ax.xaxis_date()
-#ax.xaxis.set_major_locator(plt.MaxNLocator(6))
 ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=[3,6,9,12]))
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
 plt.savefig('/home/ec2-user/plutus/plotsample002.png')
