5366060f480de93b3c165a095ed35aa557852678
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-09 17:50:02 +0530

    Update candleplot.py

--- src/candleplot.py
+++ src/candleplot.py
@@ -5,7 +5,6 @@ from mpl_finance import candlestick_ohlc
 from mpl_finance import candlestick2_ohlc
 import pandas as pd
 from datetime import datetime
-from dateutil.rrule import rrule #test001
 '''
 import fortunacommon
 '''
@@ -18,18 +17,14 @@ def plotCandlestick(df,filename):
     nDate=date2num(row.name.to_pydatetime())
     rec = nDate, row['open'], row['high'], row['low'], row['close']
     ohlc.append(rec)
-  #fig, ax = plt.subplots(figsize=(20, 15))
   fig, ax = plt.subplots()
   candlestick_ohlc(ax, ohlc, colorup='#77d879', colordown='#db3f3f')
   ax.plot_date(df.index.values, df['sma20'].values, color='b', linestyle='solid', marker=',', linewidth=1)
   ax.autoscale_view()
-  #ax.xaxis.grid(True, 'major')
-  #ax.grid(True)
   ax.fill_between(df.index.values, df['hband'].values, df['lband'].values, color='blue', alpha=0.1)
-  #ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=[0,2,4])) #test001
   ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=0)) #test001
   ax.xaxis.set_major_formatter(mdates.DateFormatter('%d\n%b'))
-  #fig.autofmt_xdate()
+  plt.tight_layout()
   fig.savefig(filename, dpi=300)
 
 '''
