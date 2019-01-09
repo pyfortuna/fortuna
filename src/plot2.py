1a4eca4b702b21e3badfbdd6ad873e101ff5d341
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-09 17:31:00 +0530

    Update plot2.py

--- src/plot2.py
+++ src/plot2.py
@@ -45,7 +45,7 @@ ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=[3,6,9,12]))
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
 plt.savefig(smaFilename, dpi=300)
 
-df1=df.tail(15)
+df1=df.tail(30)
 #cp.plotCandlestick(df1,candleFilename,'%Y-%m-%d')
 cp.plotCandlestick(df1,candleFilename)
 
