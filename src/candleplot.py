cd895b897c4de1a0e591cca1653c645cb00b5123
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-09 17:54:23 +0530

    Update candleplot.py

--- src/candleplot.py
+++ src/candleplot.py
@@ -24,7 +24,7 @@ def plotCandlestick(df,filename):
   ax.fill_between(df.index.values, df['hband'].values, df['lband'].values, color='blue', alpha=0.1)
   ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=0)) #test001
   ax.xaxis.set_major_formatter(mdates.DateFormatter('%d\n%b'))
-  plt.tight_layout()
+  fig.tight_layout()
   fig.savefig(filename, dpi=300)
 
 '''
