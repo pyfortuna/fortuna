2452175d721987cd3cacee89ed3abbc632a4535f
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-09 14:08:10 +0530

    Update plot2.py

--- src/plot2.py
+++ src/plot2.py
@@ -34,7 +34,7 @@ ax.legend()
 ax.xaxis_date()
 ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=[3,6,9,12]))
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
-plt.savefig('/home/ec2-user/plutus/plotsample002.png')
+plt.savefig('/home/ec2-user/plutus/plotsample003.png', dpi=300)
 
 # Send mail
-fc.sendMail('Plot','Plot','/home/ec2-user/plutus/plotsample002.png')
+fc.sendMail('Plot','Plot','/home/ec2-user/plutus/plotsample003.png')
