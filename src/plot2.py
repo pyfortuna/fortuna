4ae150ad1ef514c1fe7a6a6b8bfabaaead0ac955
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-09 15:26:44 +0530

    Update plot2.py

--- src/plot2.py
+++ src/plot2.py
@@ -20,10 +20,10 @@ df=df.assign(sma200=sma200)
 df=df.assign(sma20=sma20)
 df=df.dropna()
 df = df[['sma200','sma20','close']]
-df['date'] = pd.to_datetime(df.index, format="%d-%b-%Y")
-df=df.set_index('date')
+#df['date'] = pd.to_datetime(df.index, format="%d-%b-%Y")
+#df=df.set_index('date')
 print(df.head())
-print(df.tail())
+#print(df.tail())
 
 # Plot chart
 fig, ax = plt.subplots()
@@ -34,7 +34,7 @@ ax.legend()
 ax.xaxis_date()
 ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=[3,6,9,12]))
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
-plt.savefig('/home/ec2-user/plutus/plotsample003.png', dpi=1200)
+plt.savefig('/home/ec2-user/plutus/plotsample003.png', dpi=300)
 
 # Send mail
 fc.sendMail('Plot','Plot','/home/ec2-user/plutus/plotsample003.png')
