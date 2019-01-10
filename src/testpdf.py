9832dbb8f3f0cf923528065951f8c1ebda1282b1
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-10 15:23:45 +0530

    Update testpdf.py

--- src/testpdf.py
+++ src/testpdf.py
@@ -4,8 +4,10 @@ import fortunacommon as fc
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Arial", size=12)
-pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
-pdf.image('/home/ec2-user/plutus/smaplot003.png', x=10, y=20, w=100)
+pdf.cell(200, 10, txt="Ashok Leyland", ln=1, align="C")
+pdf.image('/home/ec2-user/plutus/smaplot003.png', x=10, y=20, w=200)
+pdf.add_page()
+pdf.image('/home/ec2-user/plutus/candleplot003.png', x=10, y=20, w=200)
 pdf.output('/home/ec2-user/plutus/testpdf.pdf')
 
 fc.sendMail('PDF','test pdf','/home/ec2-user/plutus/testpdf.pdf')
