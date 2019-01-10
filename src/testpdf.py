797a4d32dfc86e417d5391e797c7059a82fd9621
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-10 15:43:07 +0530

    Update testpdf.py

--- src/testpdf.py
+++ src/testpdf.py
@@ -7,8 +7,8 @@ pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Arial", size=12)
 pdf.write(5, 'Ashok Leyland')
-pdf.image('/home/ec2-user/plutus/smaplot003.png', w=150)
-pdf.image('/home/ec2-user/plutus/candleplot003.png', w=150)
+pdf.image('/home/ec2-user/plutus/smaplot003.png', x=10, y=100, w=pdf.w)
+pdf.image('/home/ec2-user/plutus/candleplot003.png', x=10, y=200, w=pdf.w)
 pdf.output('/home/ec2-user/plutus/testpdf.pdf')
 
 # Send Mail
