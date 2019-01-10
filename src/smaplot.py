1dadfe7efb356b931b5b4d8c4b6f0e0e5a436b0d
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-10 18:29:36 +0530

    Create smaplot.py

---
+++ src/smaplot.py
@@ -0,0 +1,14 @@
+
+import matplotlib.pyplot as plt
+
+def plot(df,smaFilename):
+  fig, ax = plt.subplots(figsize=(6.69,4.33)) # 170 x 110 mm = 6.69 x 4.33 in
+  ax.plot_date(df.index.values, df['sma200'].values, color='b', linestyle='solid', marker=',', linewidth=1, label='SMA/200')
+  ax.plot_date(df.index.values, df['sma20'].values, color='g', linestyle='solid', marker=',', linewidth=1, label='SMA/20')
+  ax.plot_date(df.index.values, df['close'].values, color='r', linestyle='solid', marker=',', linewidth=1, label=companyCode)
+  #ax.fill_between(df.index.values, df['hband'].values, df['lband'].values, color='blue', alpha=0.3)
+  ax.legend(frameon=False)
+  ax.xaxis_date()
+  ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=[3,6,9,12]))
+  ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
+  plt.savefig(smaFilename, dpi=300)
