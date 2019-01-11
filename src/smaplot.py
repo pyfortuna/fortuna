9ec4c2d34a7cce7581839a3d27039b3fc8396c4d
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-11 18:07:03 +0530

    Update smaplot.py

--- src/smaplot.py
+++ src/smaplot.py
@@ -11,7 +11,7 @@ def plotSMA(df,smaFilename,w,h):
   ax.xaxis_date()
   ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=[3,6,9,12]))
   ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
-  plt.savefig(smaFilename, dpi=300)
+  plt.savefig(smaFilename, dpi=300, bbox_inches='tight', pad_inches=0)
 
 def plotMACD(df,macdFilename,w,h):
   fig, ax = plt.subplots(figsize=(w,h))
@@ -20,4 +20,4 @@ def plotMACD(df,macdFilename,w,h):
   ax.xaxis_date()
   ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=[3,6,9,12]))
   ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
-  plt.savefig(macdFilename, dpi=300)
+  plt.savefig(macdFilename, dpi=300, bbox_inches='tight', pad_inches=0)
