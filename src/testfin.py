4446d137ab4ab3950e0cece25ace6badf8088be0
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-11 15:16:37 +0530

    Update testfin.py

--- src/testfin.py
+++ src/testfin.py
@@ -1,6 +1,3 @@
 import pandas as pd
-dfList=pd.read_html('https://www.moneycontrol.com/financials/abbindia/results/yearly/ABB#ABB')
-for df in dfList:
-  print('='*50)
-  print(list(df))
-  print(df.head())
+df=pd.read_html('https://www.moneycontrol.com/financials/abbindia/results/yearly/ABB#ABB')[3]
+print(df)
