7d75b92f154aaf0bdfc1086279708a096c57c7a3
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-11 17:27:10 +0530

    Update smaplot.py

--- src/smaplot.py
+++ src/smaplot.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-def plotSMA(df,smaFilename):
-  fig, ax = plt.subplots(figsize=(6.69,4.33)) # 170 x 110 mm = 6.69 x 4.33 in
+def plotSMA(df,smaFilename,w,h):
+  fig, ax = plt.subplots(figsize=(w,h))
   ax.plot_date(df.index.values, df['sma200'].values, color='b', linestyle='solid', marker=',', linewidth=1, label='SMA/200')
   ax.plot_date(df.index.values, df['sma20'].values, color='g', linestyle='solid', marker=',', linewidth=1, label='SMA/20')
   ax.plot_date(df.index.values, df['close'].values, color='r', linestyle='solid', marker=',', linewidth=1, label='Price')
@@ -12,3 +12,12 @@ def plotSMA(df,smaFilename):
   ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=[3,6,9,12]))
   ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
   plt.savefig(smaFilename, dpi=300)
+
+def plotMACD(df,macdFilename,w,h):
+  fig, ax = plt.subplots(figsize=(w,h))
+  ax.plot_date(df.index.values, df['macd'].values, color='b', linestyle='solid', marker=',', linewidth=1, label='MACD')
+  ax.legend(frameon=False)
+  ax.xaxis_date()
+  ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=[3,6,9,12]))
+  ax.xaxis.set_major_formatter(mdates.DateFormatter('%b\n%Y'))
+  plt.savefig(macdFilename, dpi=300)
