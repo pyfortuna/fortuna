c5fca76b2c05e4755818aa2b971757b00b7c0af3
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-01-17 17:54:25 +0530

    Update testpdf.py

--- src/testpdf.py
+++ src/testpdf.py
@@ -8,11 +8,11 @@ class HTML2PDF(FPDF, HTMLMixin):
 # Main Program
 pdf = HTML2PDF()
 
-table = """<table border="0" align="center" width="100%">
+table = """<table border="0" width="100%">
 <thead><tr><th width="25%">Details</th><th width="15%">Mar '18</th><th width="15%">Mar '17</th><th width="15%">Mar '16</th><th width="15%">Mar '15</th><th width="15%">Mar '14</th></tr></thead>
 <tbody>
-<tr><td>1562.59</td><td>1223.08</td><td>389.6</td><td>334.81</td><td>29.38</td></tr>
-<tr><td>5.34</td><td>4.24</td><td>1.37</td><td>1.2</td><td>0.11</td></tr>
+<tr><td>Profit</td><td>1562.59</td><td>1223.08</td><td>389.6</td><td>334.81</td><td>29.38</td></tr>
+<tr><td>EPS</td><td>5.34</td><td>4.24</td><td>1.37</td><td>1.2</td><td>0.11</td></tr>
 </tbody>
 </table>"""
 
