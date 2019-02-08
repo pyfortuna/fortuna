f5d6a540428417f992ced538974d4bad51c8bc92
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-02-08 16:02:15 +0530

    Update candleplot.py

--- src/candleplot.py
+++ src/candleplot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.dates import date2num
+from matplotlib.ticker import Formatter
 from mpl_finance import candlestick_ohlc
 from mpl_finance import candlestick2_ohlc
 import pandas as pd
@@ -8,7 +9,16 @@ from datetime import datetime
 '''
 import fortunacommon
 '''
-
+class MyFormatter(Formatter): #test002
+  def __init__(self, dates, fmt='%d\n%b'):
+    self.dates = dates
+    self.fmt = fmt
+  def __call__(self, x, pos=0):
+    ind = int(np.round(x))
+    if ind >= len(self.dates) or ind < 0:
+      return ''
+    return num2date(self.dates[ind]).strftime(self.fmt)
+      
 #def plotCandlestick(df,filename,dateFormat):
 def plotCandlestick(df,filename,w,h):
   ohlc = []
@@ -18,12 +28,16 @@ def plotCandlestick(df,filename,w,h):
     rec = nDate, row['open'], row['high'], row['low'], row['close']
     ohlc.append(rec)
   fig, ax = plt.subplots(figsize=(w,h))
+  formatter = MyFormatter(df.index.values)  #test002
+
   candlestick_ohlc(ax, ohlc, colorup='#77d879', colordown='#B72015')
-  ax.plot_date(df.index.values, df['sma20'].values, color='b', linestyle='solid', marker=',', linewidth=1)
+  #ax.plot_date(df.index.values, df['sma20'].values, color='b', linestyle='solid', marker=',', linewidth=1) #test002
+  ax.plot_date(np.arange(len(df.index.values)), df['sma20'].values, color='b', linestyle='solid', marker=',', linewidth=1)
   ax.autoscale_view()
   ax.fill_between(df.index.values, df['hband'].values, df['lband'].values, color='#b3e1f2', alpha=0.15)
   ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=0)) #test001
-  ax.xaxis.set_major_formatter(mdates.DateFormatter('%d\n%b'))
+  #ax.xaxis.set_major_formatter(mdates.DateFormatter('%d\n%b'))  #test002
+  ax.xaxis.set_major_formatter(formatter)
   fig.savefig(filename, dpi=300, bbox_inches='tight', pad_inches=0)
 
 '''
