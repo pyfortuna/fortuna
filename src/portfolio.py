995219f612876b4c8eab6e5cceee561091afbff8
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-02-11 15:12:48 +0530

    Update portfolio.py

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,17 +1,17 @@
 class Portfolio:
 	def __init__ (self):
-		self.capDr = 0
-		self.capCr = 0
-		self.trdDr = 0
-		self.trdCr = 0
-		self.brkDr = 0
-		self.brkCr = 0
-		self.invDr = 0
-		self.invCr = 0
-		self.salDr = 0
-		self.salCr = 0
-		self.cogDr = 0
-		self.cogCr = 0
+		self.capDr = 0	# Capital (DR)
+		self.capCr = 0	# Capital (CR)
+		self.trdDr = 0	# Trade (DR)
+		self.trdCr = 0	# Trade (CR)
+		self.brkDr = 0	# Brokerage (DR)
+		self.brkCr = 0	# Brokerage (CR)
+		self.invDr = 0	# Inventory (DR)
+		self.invCr = 0	# Inventory (CR)
+		self.salDr = 0	# Sales (DR)
+		self.salCr = 0	# Sales (CR)
+		self.cogDr = 0	# Cost of Goods (DR)
+		self.cogCr = 0	# Cost of Goods (CR)
 	def addCapital (self, amount):
 		self.capCr += amount
 		self.trdDr += amount
@@ -30,29 +30,31 @@ class Portfolio:
 		capBal = self.capDr - self.capCr
 		trdBal = self.trdDr - self.trdCr
 		brkBal = self.brkDr - self.brkCr
-		invBal = self.invDr - self.invCr
+		invBal = self.invDr - self.invCr			
 		salBal = self.salDr - self.salCr
 		cogBal = self.cogDr - self.cogCr
 		totBal = capBal + trdBal + brkBal + invBal + salBal + cogBal
 		return int(totBal)
+	def getcashBalance(self):
+		cashBalance = (self.capCr + self.salCr) - (self.invDr + self.brkDr)
+		return cashBalance
 	def getResults(self):
 		grossPL = self.salCr - self.cogDr
 		netPL = grossPL - self.brkDr
-		cashBalance = (self.capCr + self.salCr) - (self.invDr + self.brkDr)
-		return grossPL, netPL, cashBalance
+		return grossPL, netPL
 
 if __name__ == "__main__":
 	pf = Portfolio()
 	pf.addCapital(10000)
 	pf.buy(460,5,2.34)
-	brk=2.36+15.93
-	pf.sell(460,490,5,brk)
+	pf.sell(460,490,5,2.36)
 	b=pf.getBalance()
 	if(b==0):
 		print('BALANCESHEET : OK')
 	else:
 		print('BALANCESHEET : ERROR')
-	g,n,c=pf.getResults()
+	g,n=pf.getResults()
+	c=pf.getcashBalance()
 	print('GROSS P/L    : %6.2f' % g)
 	print('NET P/L      : %6.2f' % n)
 	print('CASH BALANCE : %6.2f' % c)
