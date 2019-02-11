4ee91037abd7df891d678625bc52da3e0fdc43da
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-02-11 17:56:48 +0530

    Update portfolio.py

--- src/portfolio.py
+++ src/portfolio.py
@@ -13,12 +13,13 @@ class Portfolio:
 		self.brkCr = 0	# Brokerage (CR)
 		self.invDr = 0	# Inventory (DR)
 		self.invCr = 0	# Inventory (CR)
+		self.invQty = 0	# Inventory (Qty)
 		self.salDr = 0	# Sales (DR)
 		self.salCr = 0	# Sales (CR)
 		self.cogDr = 0	# Cost of Goods (DR)
 		self.cogCr = 0	# Cost of Goods (CR)
 		col_names =  ['ACC', 'DESCRIPTION', 'DR', 'CR']
-		self.bsDF = pd.DataFrame(columns = col_names)
+		self.bsDF = pd.DataFrame(columns=col_names)
 	def calculateBuyBrokerage (self, price, qty):
 		totalTrade	= price * qty
 		brokerage =	0.01
@@ -40,26 +41,42 @@ class Portfolio:
 		dpCharges = 15.93
 		totalCharges = brokerage + exchangeTxnCharge + gst + securityTxnTax + sebiTurnoverFee + stampDuty + dpCharges
 		return totalCharges
+	def addBalanceSheetRecord (self, record):
+		tempDF=pd.DataFrame([record])
+		self.bsDF=self.bsDF.append(tempDF)
 	def addCapital (self, amount):
 		self.capCr += amount
 		self.trdDr += amount
-		t1=pd.DataFrame([{'ACC': 'CAP', 'DESCRIPTION': 'Adding Capital', 'DR': '', 'CR':amount}])
-		self.bsDF=self.bsDF.append(t1)
-		t2=pd.DataFrame([{'ACC': 'TRD', 'DESCRIPTION': 'Adding Capital', 'DR': amount, 'CR':''}])
-		self.bsDF=self.bsDF.append(t2)
+		self.addBalanceSheetRecord({'ACC': 'CAP', 'DESCRIPTION': 'Adding Capital', 'DR': '', 'CR':amount})
+		self.addBalanceSheetRecord({'ACC': 'TRD', 'DESCRIPTION': 'Adding Capital', 'DR': amount, 'CR':''})
 	def buy (self, buyPrice, qty):
 		brokerage = self.calculateBuyBrokerage(buyPrice, qty)
-		self.trdCr += (buyPrice*qty) + brokerage
-		self.invDr += (buyPrice*qty)
+		buyAmt = (buyPrice*qty)
+		self.trdCr += buyAmt + brokerage
+		self.invDr += buyAmt
 		self.brkDr += brokerage
+		self.invQty += qty
+		self.addBalanceSheetRecord({'ACC': 'TRD', 'DESCRIPTION': 'Buy', 'DR': '', 'CR':buyAmt})
+		self.addBalanceSheetRecord({'ACC': 'TRD', 'DESCRIPTION': 'Brokerage', 'DR': '', 'CR':brokerage})
+		self.addBalanceSheetRecord({'ACC': 'INV', 'DESCRIPTION': 'Buy', 'DR': buyAmt, 'CR':''})
+		self.addBalanceSheetRecord({'ACC': 'BRK', 'DESCRIPTION': 'Brokerage', 'DR': brokerage, 'CR':''})
 	def sell(self, buyPrice, sellPrice, qty):
 		brokerage = self.calculateSellBrokerage(sellPrice, qty)
-		self.trdDr += (sellPrice*qty)
+		sellAmt = (sellPrice*qty)
+		buyAmt = (buyPrice*qty)
+		self.trdDr += sellAmt
 		self.brkDr += brokerage
-		self.cogDr += (buyPrice*qty)
-		self.salCr += (sellPrice*qty)
+		self.cogDr += buyAmt
+		self.salCr += sellAmt
 		self.trdCr += brokerage
-		self.invCr += (buyPrice*qty)
+		self.invCr += buyAmt
+		self.invQty -= qty
+		self.addBalanceSheetRecord({'ACC': 'TRD', 'DESCRIPTION': 'Sell', 'DR': sellAmt, 'CR':''})
+		self.addBalanceSheetRecord({'ACC': 'BRK', 'DESCRIPTION': 'Sell (Brokerage)', 'DR': brokerage, 'CR':''})
+		self.addBalanceSheetRecord({'ACC': 'COG', 'DESCRIPTION': 'Sell', 'DR': buyAmt, 'CR':''})
+		self.addBalanceSheetRecord({'ACC': 'SAL', 'DESCRIPTION': 'Sell', 'DR': '', 'CR':sellAmt})
+		self.addBalanceSheetRecord({'ACC': 'TRD', 'DESCRIPTION': 'Sell (Brokerage)', 'DR': '', 'CR':brokerage})
+		self.addBalanceSheetRecord({'ACC': 'INV', 'DESCRIPTION': 'Sell', 'DR': '', 'CR':buyAmt})
 	def getBalance(self):
 		capBal = self.capDr - self.capCr
 		trdBal = self.trdDr - self.trdCr
@@ -69,6 +86,8 @@ class Portfolio:
 		cogBal = self.cogDr - self.cogCr
 		totBal = capBal + trdBal + brkBal + invBal + salBal + cogBal
 		return int(totBal)
+	def getInventoryBalance(self):
+		return int(self.invQty)
 	def getcashBalance(self):
 		cashBalance = (self.capCr + self.salCr) - (self.invDr + self.brkDr)
 		return cashBalance
@@ -84,9 +103,9 @@ class Portfolio:
 if __name__ == "__main__":
 	pf = Portfolio()
 	pf.addCapital(5000)
-	pf.printBalanceSheet()
 	pf.buy(460,5)
 	pf.sell(460,490,5)
+	pf.printBalanceSheet()
 	b=pf.getBalance()
 	if(b==0):
 		print('BALANCESHEET : OK')
@@ -94,7 +113,9 @@ if __name__ == "__main__":
 		print('BALANCESHEET : ERROR')
 	g,n=pf.getResults()
 	c=pf.getcashBalance()
+	i=pf.getInventoryBalance()
 	print('GROSS P/L    : %6.2f' % g)
 	print('NET P/L      : %6.2f' % n)
 	print('CASH BALANCE : %6.2f' % c)
+	print('INV BALANCE  : %6.2f' % i)
 	
