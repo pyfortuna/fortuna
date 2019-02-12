6a600fbf4f22fb4713d7607b9db4c156979863e5
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-02-12 12:29:25 +0530

    Update portfolio.py

--- src/portfolio.py
+++ src/portfolio.py
@@ -120,6 +120,9 @@ if __name__ == "__main__":
 	'''
 	i = InventoryList()
 	i.add(460,5)
+	i.add(470,4)
 	x=i.remove(5)
+	print('AVG PRICE : %6.2f' % x)
+	x=i.remove(4)
 	print('AVG PRICE : %6.2f' % x)
 	
