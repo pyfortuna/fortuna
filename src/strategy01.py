4571aa622cca87a2e6abfc686f948baff5fe24bd
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-02-12 19:18:39 +0530

    Update strategy01.py

--- src/strategy01.py
+++ src/strategy01.py
@@ -1,3 +1,17 @@
+import nseUtil as nu
+import datetime
+
 class Stategy01:
-  def executeStrategy(capital,df):
-    
+	def executeStrategy(capital,df):
+		print(df.tail(20))
+'''
+	Test program
+'''
+if __name__ == "__main__":
+	capital=10000
+	companyCode='RELIANCE'	
+	e = datetime.datetime.now()
+	s = e - datetime.timedelta(days=700)
+	df = nu.getHistoricPrice(companyCode,s,e)
+	s1 = Stategy01()
+	s1.executeStrategy(capital,df)
