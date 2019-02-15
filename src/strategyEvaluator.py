9d551668eba3ad962ee9c4b698d153750a9a23e4
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-02-15 11:32:49 +0530

    Update strategyEvaluator.py

--- src/strategyEvaluator.py
+++ src/strategyEvaluator.py
@@ -8,26 +8,26 @@ import portfolio as p
 	Test program
 '''
 if __name__ == "__main__":
-	capital=10000
-	companyCode='PIDILITIND'	
 	e = datetime.datetime.now()
 	s = e - datetime.timedelta(days=700)
-	df = nu.getHistoricPrice(companyCode,s,e)	
-	confDF = pd.read_table('/home/ec2-user/fortuna/fortuna/data/s01conf.tsv', header=0)
-	paramList = confDF.to_dict('records')
 	res = pd.DataFrame()
-	
-	for param in paramList:
-		# get recommendation
-		s1 = s01.Strategy01()
-		txnList = s1.executeStrategy(param,df)
-		# test recommendation
-		pf = p.Portfolio(param['id'])
-		pf.addCapital('2017-01-01',capital)
-		pf.processTxnList(txnList)
-		pf.printSummary()
-		#pf.printBalanceSheet()
-		#pf.printBalanceSheet('TRD')
-		resultDF=pf.getResult()
-		res = res.append(resultDF)
+	confDF = pd.read_table('/home/ec2-user/fortuna/fortuna/data/s01conf.tsv', header=0)
+	companyList = confDF['companyCode'].unique()
+	for companyCode in companyList:
+		compConfDF = confDF.loc[confDF['companyCode']==companyCode][['id','capital','smaDays','trendStrength']]
+		df = nu.getHistoricPrice(companyCode,s,e)		
+		paramList = compConfDF.to_dict('records')
+		for param in paramList:
+			# get recommendation
+			s1 = s01.Strategy01()
+			txnList = s1.executeStrategy(param,df)
+			# test recommendation
+			pf = p.Portfolio("%s:%d" % (companyCode, param['id']))
+			pf.addCapital('2017-01-01',param['capital'])
+			pf.processTxnList(txnList)
+			#pf.printSummary()
+			#pf.printBalanceSheet()
+			#pf.printBalanceSheet('TRD')
+			resultDF=pf.getResult()
+			res = res.append(resultDF)
 	print(res)
