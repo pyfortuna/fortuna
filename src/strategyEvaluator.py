7b0d6df68ab1d875ed44e1f835a43358497a941d
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-02-28 17:02:30 +0530

    Update strategyEvaluator.py

--- src/strategyEvaluator.py
+++ src/strategyEvaluator.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
 	companyList = confDF['companyCode'].unique()
 	for companyCode in companyList:
 		compConfDF = confDF.loc[confDF['companyCode']==companyCode][['id','capital','smaDays','trendStrength']]
-		print('DEBUG : Downloading NSE data for %s' % companyCode)
 		df = nu.getHistoricPrice(companyCode,s,e)		
 		paramList = compConfDF.to_dict('records')
 		for param in paramList:
@@ -27,12 +26,13 @@ if __name__ == "__main__":
 			pf = p.Portfolio(param['id'])
 			pf.addCapital('2017-01-01',param['capital'])
 			pf.processTxnList(txnList)
-			#pf.printSummary()
+			pf.printSummary()
 			#pf.printBalanceSheet()
 			#pf.printBalanceSheet('TRD')
-			resultDF=pf.getResult()
+			resultDF=pf.getResult()			
 			res = res.append(resultDF)
 	print(res)
+	res_out = pd.merge(confDF,res,left_on='id',right_on='ID',how='inner')
 	outputFilename = '/home/ec2-user/plutus/s01output.csv'
-	res.to_csv(outputFilename)
+	res_out.to_csv(outputFilename)
 	fc.sendMail('S01','S01',outputFilename)
