9507582d91c32b463d67d9ae0923f824b3267bc2
pyfortuna <45413790+pyfortuna@users.noreply.github.com>
2019-03-28 18:05:38 +0530

    Create nseHistDownload.py

---
+++ src/nseHistDownload.py
@@ -0,0 +1,19 @@
+import nseUtil as nu
+import datetime
+import pandas as pd
+
+'''
+	Test program
+'''
+if __name__ == "__main__":
+	e = datetime.datetime.now()
+	s = e - datetime.timedelta(days=700)
+	res = pd.DataFrame()
+	confDF = pd.read_table('/home/ec2-user/fortuna/fortuna/data/nselist.tsv', header=0)
+	companyList = confDF['companyCode'].unique()
+	for companyCode in companyList:
+		df = nu.getHistoricPrice(companyCode,s,e)
+    output_filename = '/home/ec2-user/plutus/nsedata/%s.csv' % companyCode
+    df.to_csv(output_filename)
+    print('DEBUG : Saved data for %s' % companyCode)
+print('DEBUG : Completed')
